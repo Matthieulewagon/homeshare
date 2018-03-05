f0cc3e96736b101047fd7af4cafa0add8eb4d06b
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-03-05 17:42:53 +0100

    translation + creation pages fr

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,5 @@ Rails.application.routes.draw do
   get 'shared/_navbar'
 
   root to: 'pages#home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
