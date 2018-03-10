ec775d3a4e99063a37ab52a01c9441136c6e05a3
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-03-10 16:30:39 +0100

    sign up working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
@@ -14,5 +15,13 @@ class ApplicationController < ActionController::Base
   end
   def default_url_options
       { locale: I18n.locale }
+  end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, :zip, :telephone, :mobile, :country, :email, :organisation, :address, :state])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :city, :zip, :telephone, :mobile, :country, :email, :organisation, :address, :state])
   end
 end
