1f81f4e76f0e8b4a1ea9f2c3a15ce35f7cabf2b8
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-03-16 11:47:18 +0100

    mail delted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
 private
 
   def send_welcome_email
-    UserMailer.welcome(self).deliver_now
+    # UserMailer.welcome(self).deliver_now
   end
 end
